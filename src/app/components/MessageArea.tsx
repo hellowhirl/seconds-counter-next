c3e6f9428e0303783d6013a28a81b629556dd4c9
Anthony Avila <anthony.p.avila@gmail.com>
2023-05-31 10:41:06 +0900

    componentize codes

---
+++ src/app/components/MessageArea.tsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import styles from "../page.module.css";
+
+export interface MessageAreaProps {
+  goalTime: number;
+  timeDifference: number | null;
+  isRunning: boolean;
+}
+
+const MessageArea: React.FC<MessageAreaProps> = ({
+  goalTime,
+  timeDifference,
+  isRunning,
+}) => {
+  return (
+    <div className={styles.topArea}>
+      <h1 className={styles.gameInstructions}>
+        Press STOP when the timer reaches {goalTime / 1000} seconds!
+      </h1>
+      {!isRunning && (
+        <div className={styles.messageArea}>
+          {timeDifference !== null && (
+            <p>
+              <b>{`${(timeDifference / 1000).toFixed(2)}`}</b>{" "}
+              <u>seconds close</u>
+            </p>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MessageArea;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
 import create from "zustand";
+import MessageArea from "./components/MessageArea";
 
 type GameState = {
   timeElapsed: number;
@@ -77,7 +78,184 @@ const useGameStore = create<GameState>((set) => ({
   },
 }));
 
-export default function Home() {
+interface TimerAreaProps {
+  timeElapsed: number;
+  isRunning: boolean;
+  handleStartStop: () => void;
+}
+
+const TimerArea: React.FC<TimerAreaProps> = ({
+  timeElapsed,
+  isRunning,
+  handleStartStop,
+}) => {
+  return (
+    <div className={styles.timerArea}>
+      <div className={styles.timerBox}>
+        <div
+          className={`${styles.timeElapsed} ${
+            timeElapsed >= 1800 && isRunning ? `${styles.fadeOut}` : ""
+          }`}
+        >
+          <div className={styles.seconds}>
+            {Math.floor(timeElapsed / 1000) < 10 ? 0 : ""}
+            {Math.floor(timeElapsed / 1000)}
+          </div>
+          .
+          <div className={styles.milliseconds}>
+            {Math.round(
+              Math.abs(timeElapsed - Math.floor(timeElapsed / 1000) * 1000) *
+                0.1
+            ) < 10
+              ? "0"
+              : ""}
+            {Math.round(
+              Math.abs(timeElapsed - Math.floor(timeElapsed / 1000) * 1000) *
+                0.1
+            )}
+          </div>
+        </div>
+      </div>
+      <button className={styles.startStopButton} onClick={handleStartStop}>
+        {isRunning ? "STOP" : "START"}
+      </button>
+    </div>
+  );
+};
+
+interface OptionsProps {
+  secondsOptions: number[];
+  selectedOption: string;
+  setGoalTime: (time: number) => void;
+  handleOptionClick: (option: string) => void;
+  setShowOptions: (arg0: boolean) => void;
+  setShowTimer: (arg0: boolean) => void;
+  setShowSettingsButtons: (arg0: boolean) => void;
+  setShowMessageArea: (arg0: boolean) => void;
+}
+
+const Options: React.FC<OptionsProps> = ({
+  secondsOptions,
+  selectedOption,
+  setGoalTime,
+  handleOptionClick,
+  setShowOptions,
+  setShowTimer,
+  setShowSettingsButtons,
+  setShowMessageArea,
+}) => {
+  return (
+    <>
+      <div
+        className={styles.arrowLeft}
+        onClick={() => {
+          setShowOptions(false);
+          setShowTimer(true);
+          setShowSettingsButtons(true);
+          setShowMessageArea(true);
+        }}
+      ></div>
+      <h2 className={styles.pageTitle}>OPTIONS</h2>
+      <div className={styles.optionsScreen}>
+        <h3 className={styles.optionTitle}>Select goal time</h3>
+        <div className={styles.optionsTimes}>
+          {secondsOptions.map((item, i) => (
+            <div className={styles.optionsTimesContainer}>
+              <button
+                className={`${styles.option} ${
+                  selectedOption === `option${i}` ? `${styles.selected}` : ""
+                }`}
+                onClick={() => {
+                  handleOptionClick(`option${i}`);
+                  setGoalTime(item * 1000);
+                }}
+              >
+                {item}
+                <span className={styles.spacer}></span>s
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+interface RecordsProps {
+  records: {
+    goalTime: number;
+    resultTime: number;
+    difference: number;
+  }[];
+  setShowRecords: (arg1: boolean) => void;
+  setShowTimer: (arg1: boolean) => void;
+  setShowSettingsButtons: (arg1: boolean) => void;
+  setShowMessageArea: (arg1: boolean) => void;
+}
+
+const Records: React.FC<RecordsProps> = ({
+  records,
+  setShowRecords,
+  setShowTimer,
+  setShowSettingsButtons,
+  setShowMessageArea,
+}) => {
+  return (
+    <>
+      <div
+        className={styles.arrowLeft}
+        onClick={() => {
+          setShowRecords(false);
+          setShowTimer(true);
+          setShowSettingsButtons(true);
+          setShowMessageArea(true);
+        }}
+      ></div>
+      <h2 className={styles.pageTitle}>RECORDS</h2>
+      <div className={styles.tableOutter}>
+        <table className={styles.recordsTable}>
+          <thead>
+            <tr>
+              <th>GOAL</th>
+              <th>RESULT</th>
+              <th>GAP</th>
+            </tr>
+          </thead>
+          <tbody>
+            {records.map((record, index) => (
+              <tr key={index}>
+                <td>
+                  {record.goalTime / 1000}
+                  <span className={styles.spacer}></span>s
+                </td>
+                <td>{(record.resultTime / 1000).toFixed(2)}</td>
+                <td>
+                  {((record.goalTime - record.resultTime) / 1000).toFixed(2)}
+                </td>
+              </tr>
+            ))}
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      {records.length === 0 && <div>No records yet</div>}
+    </>
+  );
+};
+
+const AdContainer: React.FC = () => {
+  return (
+    <div className={styles.adContainer}>
+      <div className={styles.adArea}>Ad space</div>
+    </div>
+  );
+};
+
+const HomePage: React.FC = () => {
   const {
     timeElapsed,
     isRunning,
@@ -117,64 +295,48 @@ export default function Home() {
 
   const [selectedOption, setSelectedOption] = useState("option1");
 
-  const handleOptionClick = (option: React.SetStateAction<string>) => {
+  const handleOptionClick = (option: string) => {
     setSelectedOption(option);
   };
 
   return (
     <div className={styles.gameContainer}>
       {showMessageArea && (
-        <div className={styles.topArea}>
-          <h1 className={styles.gameInstructions}>
-            Press STOP when timer reaches {goalTime / 1000} seconds!
-          </h1>
-
-          {!isRunning && (
-            <div className={styles.messageArea}>
-              {timeDifference !== null && (
-                <p>
-                  <b>{`${(timeDifference / 1000).toFixed(2)}`}</b>{" "}
-                  <u>seconds close</u>
-                </p>
-              )}
-            </div>
-          )}
-        </div>
+        <MessageArea
+          goalTime={goalTime}
+          timeDifference={timeDifference}
+          isRunning={isRunning}
+        />
       )}
       {showTimer && (
-        <div className={styles.timerArea}>
-          <div className={styles.timerBox}>
-            <div
-              className={`${styles.timeElapsed} ${
-                timeElapsed >= 1800 && isRunning ? `${styles.fadeOut}` : ""
-              }`}
-            >
-              <div className={styles.seconds}>
-                {Math.floor(timeElapsed / 1000) < 10 ? 0 : ""}
-                {Math.floor(timeElapsed / 1000)}
-              </div>
-              .
-              <div className={styles.milliseconds}>
-                {Math.round(
-                  Math.abs(
-                    timeElapsed - Math.floor(timeElapsed / 1000) * 1000
-                  ) * 0.1
-                ) < 10
-                  ? "0"
-                  : ""}
-                {Math.round(
-                  Math.abs(
-                    timeElapsed - Math.floor(timeElapsed / 1000) * 1000
-                  ) * 0.1
-                )}
-              </div>
-            </div>
-          </div>
-          <button className={styles.startStopButton} onClick={handleStartStop}>
-            {isRunning ? "STOP" : "START"}
-          </button>
-        </div>
+        <TimerArea
+          timeElapsed={timeElapsed}
+          isRunning={isRunning}
+          handleStartStop={handleStartStop}
+        />
       )}
+      {showOptions && (
+        <Options
+          secondsOptions={secondsOptions}
+          selectedOption={selectedOption}
+          setGoalTime={setGoalTime}
+          handleOptionClick={handleOptionClick}
+          setShowOptions={setShowOptions}
+          setShowTimer={setShowTimer}
+          setShowSettingsButtons={setShowSettingsButtons}
+          setShowMessageArea={setShowMessageArea}
+        />
+      )}
+      {showRecords && (
+        <Records
+          records={records}
+          setShowRecords={setShowRecords}
+          setShowTimer={setShowTimer}
+          setShowSettingsButtons={setShowSettingsButtons}
+          setShowMessageArea={setShowMessageArea}
+        />
+      )}
+      <AdContainer />
       <div className={styles.options}>
         {showSettingsButtons && (
           <button
@@ -205,93 +367,8 @@ export default function Home() {
           </button>
         )}
       </div>
-      {showOptions && (
-        <>
-          <div
-            className={styles.arrowLeft}
-            onClick={() => {
-              setShowOptions(false);
-              setShowTimer(true);
-              setShowSettingsButtons(true);
-              setShowMessageArea(true);
-            }}
-          ></div>
-          <h2 className={styles.pageTitle}>OPTIONS</h2>
-          <div className={styles.optionsScreen}>
-            <h3 className={styles.optionTitle}>Select goal time</h3>
-            <div className={styles.optionsTimes}>
-              {secondsOptions.map((item, i) => (
-                <div className={styles.optionsTimesContainer}>
-                  <button
-                    className={`${styles.option} ${
-                      selectedOption === `option${i}`
-                        ? `${styles.selected}`
-                        : ""
-                    }`}
-                    onClick={() => {
-                      handleOptionClick(`option${i}`);
-                      setGoalTime(item * 1000);
-                    }}
-                  >
-                    {item}
-                    <span className={styles.spacer}></span>s
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>
-        </>
-      )}
-      {showRecords && (
-        <>
-          <div
-            className={styles.arrowLeft}
-            onClick={() => {
-              setShowRecords(false);
-              setShowTimer(true);
-              setShowSettingsButtons(true);
-              setShowMessageArea(true);
-            }}
-          ></div>
-          <h2 className={styles.pageTitle}>RECORDS</h2>
-          <div className={styles.tableOutter}>
-            <table className={styles.recordsTable}>
-              <thead>
-                <tr>
-                  <th>GOAL</th>
-                  <th>RESULT</th>
-                  <th>GAP</th>
-                </tr>
-              </thead>
-              <tbody>
-                {records.map((record, index) => (
-                  <tr key={index}>
-                    <td>
-                      {record.goalTime / 1000}
-                      <span className={styles.spacer}></span>s
-                    </td>
-                    <td>{(record.resultTime / 1000).toFixed(2)}</td>
-                    <td>
-                      {((record.goalTime - record.resultTime) / 1000).toFixed(
-                        2
-                      )}
-                    </td>
-                  </tr>
-                ))}
-                <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          {records.length === 0 && <div>No records yet</div>}
-        </>
-      )}
-      <div className={styles.adContainer}>
-        <div className={styles.adArea}>Ad space</div>
-      </div>
     </div>
   );
-}
+};
+
+export default HomePage;
